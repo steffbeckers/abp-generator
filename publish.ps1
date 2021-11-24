a9b81bcb46daa5998eab2d830fda57cd2c92b027
Steff Beckers <steff@steffbeckers.eu>
2021-11-24 01:18:33 +0100

    publish update

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,18 @@
-param ([string] $version, [string] $apiKey)
+param ([string] $apiKey)
+
+# Increment version
+$projectPath = "SteffBeckers.Abp.Generator.csproj";
+[xml] $csproj = Get-Content $projectPath;
+
+$version = $csproj.Project.PropertyGroup.Version.Split(".");
+$major = [int]$version[0];
+$minor = [int]$version[1];
+$patch = [int]$version[2];
+
+$patch = $patch + 1;
+
+$csproj.Project.PropertyGroup.Version = "$major.$minor.$patch";
+$csproj.Save($projectPath);
 
 dotnet pack -c Release
-dotnet nuget push bin/Release/SteffBeckers.Abp.Generator.$version.nupkg --api-key $apiKey --source https://api.nuget.org/v3/index.json
+dotnet nuget push bin/Release/SteffBeckers.Abp.Generator.$major.$minor.$patch.nupkg --api-key $apiKey --source https://api.nuget.org/v3/index.json
